bf6254d42e646c48ff3ed5191be70cfef5dcd0af
Anthony Mace <amace93@gmail.com>
2014-07-16 19:46:02 -0700

    registering tableCells

--- MedianApp/AMMSemesterTVC.m
+++ MedianApp/AMMSemesterTVC.m
@@ -28,6 +28,12 @@
 {
     [super viewDidLoad];
     
+    //Custom viewDidLoad methods
+    UINib *nib = [UINib nibWithNibName:@"AMMSchoolClassCell" bundle:nil];
+    [self.tableView registerNib:nib forCellReuseIdentifier:@"AMMSchoolClassCell"];
+    
+    
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -45,28 +51,26 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [[[AMMClassStore classStore] allClasses] count];
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AMMSchoolClassCell" forIndexPath:indexPath];
     
     // Configure the cell...
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
